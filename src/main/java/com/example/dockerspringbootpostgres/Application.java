188caf6b9abadafc622372c009dfda0a9f91fea0
BocharovAnton <78930867+BocharovAnton@users.noreply.github.com>
2021-11-30 16:45:20 +0300

    percents were added

--- src/main/java/com/example/dockerspringbootpostgres/Application.java
+++ src/main/java/com/example/dockerspringbootpostgres/Application.java
@@ -15,6 +15,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import java.util.Map.Entry;
+
 
 @SpringBootApplication
 public class Application {
@@ -44,39 +46,95 @@ public class Application {
     @Autowired
     private RestHighLevelClient client;
     @EventListener(ApplicationReadyEvent.class)
+
     public void runAfterStartup(){
-        System.out.println("111");
+        int[] startDate = new int[5];
+        int[] endDate = new int[5];
+        String str;
         entryData();
         System.out.println("-------------------");
-        lab1();
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("startDate: ");
+//        for (int i = 0; i < 5; i++) {
+//            startDate[i] = in.nextInt(); // Заполняем массив элементами, введёнными с клавиатуры
+//        }
+//        System.out.print("endDate: ");
+//        for (int i = 0; i < 5; i++) {
+//            endDate[i] = in.nextInt(); // Заполняем массив элементами, введёнными с клавиатуры
+//        }
+//        System.out.print("string: ");
+//        str = in.next();
+        //УБРАТЬ ПОСЛЕ ТЕСТОВ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        startDate = new int[]{2020, 1, 1, 0, 0};//УБРАТЬ ПОСЛЕ ТЕСТОВ!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        endDate = new int[]{2022, 1, 1, 0, 0};//УБРАТЬ ПОСЛЕ ТЕСТОВ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //УБРАТЬ ПОСЛЕ ТЕСТОВ!!!!!!!
+        str = "несет в себе";
+        lab1(str, startDate, endDate);
+    }
+    public static HashMap<Integer, Integer> sortByValue(HashMap<Integer, Integer> hm)
+    {
+        // Create a list from elements of HashMap
+        List<Map.Entry<Integer, Integer> > list =
+                new LinkedList<Map.Entry<Integer, Integer> >(hm.entrySet());
+
+        // Sort the list
+        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer> >() {
+            public int compare(Map.Entry<Integer, Integer> o1,
+                               Map.Entry<Integer, Integer> o2)
+            {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        // put data from sorted list to hashmap
+        HashMap<Integer, Integer> temp = new LinkedHashMap<Integer, Integer>();
+        for (Map.Entry<Integer, Integer> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+        return temp;
     }
     HashMap<Integer, Integer> studentsAttendance= new HashMap<>();
-    public void lab1(){
+    HashMap<Integer, Integer> studentsAttendanceWithPercentage= new HashMap<>();
+    public void lab1(String textEntry, int[] startDate, int[]endDate){
         List<LectureFullText> lectureFullTextList = service.elasticSearch("несет в себе");
         for(LectureFullText lft:lectureFullTextList) {
             Lecture lecture = lectureRepository.findById(lft.getId());
-            List <Timetable> timetableList = timetableRepository.findAllByLectureAndDateBetween(lecture, LocalDateTime.of(2021, 1, 1, 0, 0), LocalDateTime.of(2022, 1, 1, 0, 0));
+            List <Timetable> timetableList = timetableRepository.findAllByLectureAndDateBetween(
+                    lecture
+                    , LocalDateTime.of(startDate[0],startDate[1],startDate[2],startDate[3],startDate[4])
+                    , LocalDateTime.of(endDate[0],endDate[1],endDate[2],endDate[3],endDate[4]));
             for(Timetable ttl :timetableList) {
                 List<Attendance> attendanceList = attendanceRepository.findAllByTimetable(ttl);
                 for(Attendance al:attendanceList) {
-                    if(al.getPresence()==true){
-                        if(studentsAttendance.get(al.getStudent()) != null) {
-                            studentsAttendance.put(al.getStudent().getId(), studentsAttendance.get(al.getStudent()) + 1);
+                    if(studentsAttendance.get(al.getStudent().getId()) != null) { //проверяем есть ли запись в map
+                        if (al.getPresence()) {//проверяем посещал ли он назначенную ему пару
+                            studentsAttendance.put(al.getStudent().getId(), studentsAttendance.get(al.getStudent().getId()) + 1);
                         }
-                        else{
-                            studentsAttendance.put(al.getStudent().getId(), 1);
-                        }
+                        studentsAttendanceWithPercentage.put(al.getStudent().getId(), studentsAttendanceWithPercentage.get(al.getStudent().getId()) + 1);
                     }
                     else{
-                        studentsAttendance.put(al.getStudent().getId(), 0);
+                        if (al.getPresence()) {
+                            studentsAttendance.put(al.getStudent().getId(), 1);
+                        } else {
+                            studentsAttendance.put(al.getStudent().getId(), 0);
+                        }
+                        studentsAttendanceWithPercentage.put(al.getStudent().getId(), 1);
                     }
                 }
             }
         }
-        System.out.println(studentsAttendance);
-        TreeMap<Integer, Integer> sorted = new TreeMap<>();
-        sorted.putAll(studentsAttendance);
-        System.out.println(sorted);
+        for(Integer i:studentsAttendance.keySet()){
+            studentsAttendance.put(i, studentsAttendance.get(i)*100/studentsAttendanceWithPercentage.get(i));
+        }
+        studentsAttendance  = sortByValue(studentsAttendance);
+
+
+
+        for(Integer i:studentsAttendance.keySet()){
+            System.out.printf(studentRepository.findById(i).toString());
+            System.out.println(studentsAttendance.get(i));
+            System.out.println("---------");
+        }
     }
     public void entryData(){
         Timestamp ts = new Timestamp(System.currentTimeMillis());
@@ -152,7 +210,7 @@ public class Application {
 
         Timetable newTimetable = new Timetable();
         newTimetable.setDate(LocalDateTime.now());
-        newTimetable.setLecture(newLecture);
+        newTimetable.setLecture(newLecture1);
         System.out.println(("Saving new Attendance..."));
         this.timetableRepository.save(newTimetable);
 
@@ -161,6 +219,12 @@ public class Application {
         newTimetable1.setLecture(newLecture1);
         System.out.println(("Saving new Attendance..."));
         this.timetableRepository.save(newTimetable1);
+
+        Timetable newTimetable2 = new Timetable();
+        newTimetable2.setDate(LocalDateTime.now());
+        newTimetable2.setLecture(newLecture);
+        System.out.println(("Saving new Attendance..."));
+        this.timetableRepository.save(newTimetable2);
 
         Attendance newAttendance = new Attendance();
         newAttendance.setPresence(true);
@@ -182,6 +246,13 @@ public class Application {
         newAttendance2.setTimetable(newTimetable1);
         System.out.println(("Saving new Attendance..."));
         this.attendanceRepository.save(newAttendance2);
+
+        Attendance newAttendance3 = new Attendance();
+        newAttendance3.setPresence(false);
+        newAttendance3.setStudent(newStudent);
+        newAttendance3.setTimetable(newTimetable2);
+        System.out.println(("Saving new Attendance..."));
+        this.attendanceRepository.save(newAttendance3);
 
         System.out.println(studentRepository.findAll());
         System.out.println(groupRepository.findAll());
